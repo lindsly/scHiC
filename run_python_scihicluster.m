d40f82a26651fcb52b01912e413182dfb2fded01
lindsly <lindsly@umich.edu>
2020-04-01 14:23:52 -0400

    running python in MATLAB fixed 2

--- run_python_scihicluster.m
+++ run_python_scihicluster.m
@@ -1,7 +1,14 @@
 %% python test
 
+% addpath(genpath('HiCluster'))
+
 commandStr = 'py "C:\Users\lindsly\Google Drive\UMICH\Research\Single Cell Hi-C\Matlab\HiCluster\HiCluster\schicluster\single_cell.py" Mymatrix.txt';
 [status, commandOut] = system(commandStr);
 if status==0
-    fprintf('squared result is %d\n',str2num(commandOut));
+    disp('Imputation complete');
 end
+
+load('imputed_matrix.mat')
+
+figure
+imagesc(log(Q))
